818ec6c1d15084d3fa7106d74c9ca6e2a01186bc
ShipraAgrawal230 <shipraagrawal230@gmail.com>
2026-06-18 20:57:38 +0530

    make app ready for deployment

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -2,15 +2,15 @@ import express from 'express'
 
 const router=express.Router()
 
-router.get("/api/auth/signup",(req,res)=>{
+router.get("/signup",(req,res)=>{
     res.send("signup request")
 })
 
-router.get("/api/auth/login",(req,res)=>{
+router.get("/login",(req,res)=>{
     res.send("login request")
 })
 
-router.get("/api/auth/logout",(req,res)=>{
+router.get("/logout",(req,res)=>{
     res.send("logout request")
 })
 export default router
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,14 +2,24 @@ import express from 'express'
 import dotenv from 'dotenv'
 import authRouter from './routes/auth.route.js'
 import messageRouter from './routes/message.route.js'
+import path from 'path'
 
 dotenv.config()
 
 const app=express()
+const __dirname=path.resolve()
 
 const port=process.env.PORT || 5000
 
 app.use("/api/auth",authRouter)
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+    app.get("*",(_,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"))
+    })
+}
 
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`)
